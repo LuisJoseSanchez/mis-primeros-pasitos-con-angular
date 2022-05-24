2b5f613d4f7709f45748e99bb867b76fdced9e5b
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-05-24 14:26:18 +0200

    Actualizando ejemplos del capítulo 3

--- mi-proyecto/src/app/ejemplos-interpolacion/ejemplos-interpolacion.component.ts
+++ mi-proyecto/src/app/ejemplos-interpolacion/ejemplos-interpolacion.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+type Persona = {
+  nombre: string;
+  puesto: string;
+  sueldo: number;
+}
+
 @Component({
   selector: 'app-ejemplos-interpolacion',
   templateUrl: './ejemplos-interpolacion.component.html',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EjemplosInterpolacionComponent implements OnInit {
 
+  x: number = 0;
+  y: number = 0;
+  palabra: string = "";
+  persona: Persona = {
+    nombre: '',
+    puesto: '',
+    sueldo: 0
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+    this.x = 23;
+    this.y = 9;
+    this.palabra = "meetup";
+    this.persona = {
+      nombre: "Alan Brito",
+      puesto: "desarrollador full-stack",
+      sueldo: 2000
+    };
   }
 
 }
