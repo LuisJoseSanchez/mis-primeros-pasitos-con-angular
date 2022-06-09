d618f67a1d810a575665c80609bcbe99a54ccfed
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-06-09 13:19:09 +0200

    Actualizando ejemplos del capítulo 8

--- ngfor-ngif/src/app/ejemplos-capitulo08/ejemplos-capitulo08.component.ts
+++ ngfor-ngif/src/app/ejemplos-capitulo08/ejemplos-capitulo08.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EjemplosCapitulo08Component implements OnInit {
 
+  temperatura = 20;
+
   constructor() { }
 
   ngOnInit(): void {
