8aac60950359129109c757ade8139f4da326bfab
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-06-11 13:27:30 +0200

    Actualizando ejemplos del capítulo 10

--- lista-de-tareas/src/app/app.module.ts
+++ lista-de-tareas/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent,
+    FormularioComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
---
+++ lista-de-tareas/src/app/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.scss']
+})
+export class ListadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
